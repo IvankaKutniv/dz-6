499d70f0899dd51f94a6b892b8587dc648909305
IvankaKutniv <146002585+IvankaKutniv@users.noreply.github.com>
2024-06-02 22:16:09 +0300

    added homework6 one more time

--- First/src/main/java/org/example/App.java
+++ First/src/main/java/org/example/App.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Random;
+import java.util.List;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Iterator;
 
 /**
  * Hello world!
@@ -11,31 +16,54 @@ import java.util.Random;
  */
 public class App
 {
-    public static void main( String[] args )    {
+//    public static void main( String[] args )    {
+//
+//        String[] cities = {"London", "Lviv", "New York"};
+//        String searchString = "Malaga";
+//
+//        boolean result = isElementInArray(cities, searchString);
+//        System.out.println("Is the city in the list? " + result);
+//    }
+//
+//    public static boolean isElementInArray(String[] array, String searchString) {
+//
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i].equals(searchString)) {
+//                return true;
+//            }
+//        }
+//
+//        for (String element : array) {
+//            if (element.equals(searchString)) {
+//                return true;
+//            }
+//        }
+//        return false;
+//    }
 
-        String[] cities = {"London", "Lviv", "New York"};
-        String searchString = "Malaga";
 
-        boolean result = isElementInArray(cities, searchString);
-        System.out.println("Is the city in the list? " + result);
+    public static void main(String[] args) {
+        List<String> cities = Arrays.asList("London", "Lviv", "Lviv", "New York", "London");
+
+        System.out.println("Set of unique cities:");
+        removeDuplicatesAndPrintSet(cities);
     }
 
-    public static boolean isElementInArray(String[] array, String searchString) {
+    public static void removeDuplicatesAndPrintSet(List<String> list) {
 
-        for (int i = 0; i < array.length; i++) {
-            if (array[i].equals(searchString)) {
-                return true;
-            }
+        Set<String> uniqueSet = new HashSet<>(list);
+
+
+        for (String element : uniqueSet) {
+            System.out.println(element);
         }
 
-        for (String element : array) {
-            if (element.equals(searchString)) {
-                return true;
-            }
+
+        Iterator<String> iterator = uniqueSet.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
         }
-        return false;
     }
-
 }
 
 
